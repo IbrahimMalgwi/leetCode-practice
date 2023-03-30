6def6771b62b011f577656c15c6a7c086b214eb0
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2023-03-30 11:41:17 +0100

    FizzBuzz and Palindrome

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class FizzBuzz {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    static public List<String> fizzBuzz(int number){
+        List<String> result = new ArrayList<>();
+
+        for (int i = 1; i <= number ; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(Integer.toString(i));
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(50));
+    }
+    //time complexity o(n)
+    //space complexity O(n)
+
 }
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Palindrome {
+public class Palindrome {
+    static public boolean isPalindrome(int number) {
+        if(number < 0 || (number % 10 == 0 && number != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while(number > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + number % 10;
+            number /= 10;
+        }
+
+        return number == revertedNumber || number == revertedNumber/10;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(12321));
+    }
+
 }
